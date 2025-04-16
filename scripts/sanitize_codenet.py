0fe13a48a76b0e828e7b8fda1574b2ebcc93b649
Fradow Liu <zhiyuanliu@smail.nju.edu.cn>
2025-04-16 14:51:35 +0800

    Sanitize transformed code in CodeNet

---
+++ scripts/sanitize_codenet.py
@@ -0,0 +1,15 @@
+import re
+
+import jsonlines
+
+if __name__ == '__main__':
+  src = 'data/CodeNet/result/codenet_deepseekcoder.jsonl'
+  with jsonlines.open(src) as reader:
+    objects = [obj for obj in reader]
+  for object in objects:
+    # eliminate everything before the first import statement in Java
+    object['result']['file_name'] = re.sub(r'^.*?(import.*)', r'\1', object['result']['file_name'], flags=re.DOTALL)
+  dst = 'data/CodeNet/result/codenet_deepseekcoder_clean.jsonl'
+  with jsonlines.open(dst, 'w') as writer:
+    for object in objects:
+      writer.write(object)
