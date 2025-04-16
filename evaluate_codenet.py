c116ac13e9e15c6cfd27deec77f3c7cb6b40e58f
Fradow Liu <zhiyuanliu@smail.nju.edu.cn>
2025-04-16 17:31:47 +0800

    Support evaluation of artificial code in CodeNet-RMV

--- evaluate_codenet.py
+++ evaluate_codenet.py
@@ -1,5 +1,10 @@
+import argparse
+import re
+from itertools import groupby
+from operator import itemgetter
 from pprint import pformat
-from typing import Sequence
+from random import sample
+from typing import NamedTuple, Sequence
 
 import jsonlines
 
@@ -9,17 +14,77 @@ from src.translate import load_model, translate_with_model
 from src.utils import average_codebleu_score, logger, parse_args
 
 
-def _load_snippets(path: str) -> Sequence[Snippet]:
-  with jsonlines.open(path) as reader:
+class Arguments(NamedTuple):
+  model: str
+  src_lang: str
+  dst_lang: str
+  generator: str
+  transformer: str
+  gpu_id: int = -1
+  num_snippets: int = -1
+
+
+def parse_args() -> Arguments:
+  default_src_lang = 'java'
+  default_dst_lang = 'cpp'
+  generators = ['claude35sonnet', 'deepseekcoder', 'gpt4o', 'wizardcoder', 'human']
+  transformers = ['claude35sonnet', 'deepseekcoder', 'gpt4o', 'wizardcoder', 'egsi', 'codebuff']
+  parser = argparse.ArgumentParser(description='Code translation evaluation tool.'
+                                               'All the datasets are evaluated by default.')
+  parser.add_argument('-m', '--model', type=str,
+                      required=True,
+                      help='Specify the model to use.')
+  parser.add_argument('--src-lang', default=default_src_lang, type=str,
+                      choices=['java', 'cpp'],
+                      help=f'Specify the source language, {default_src_lang} by default.')
+  parser.add_argument('--dst-lang', default=default_dst_lang,type=str,
+                      choices=['c', 'cpp', 'cs', 'go', 'java', 'js', 'kotlin', 'php', 'python', 'ruby', 'rust'],
+                      help=f'Specify the destination language, {default_dst_lang} by default.')
+  parser.add_argument('-g', '--generator', type=str,
+                      required=True, choices=generators,
+                      help='Specify the code generator.')
+  parser.add_argument('-t', '--transformer', type=str,
+                      required=True, choices=transformers,
+                      help='Specify the code transformer.')
+  parser.add_argument('-i', '--gpu-id', type=int, default=-1,
+                      help='Specify the GPU to use.')
+  parser.add_argument('-n', '--num-snippets', type=int, default=-1,
+                      help='Limit the number of snippets to test.')
+  args = parser.parse_args()
+  return Arguments(
+    model=args.model,
+    src_lang=args.src_lang,
+    dst_lang=args.dst_lang,
+    generator=args.generator,
+    transformer=args.transformer,
+    gpu_id=args.gpu_id,
+    num_snippets=args.num_snippets,
+  )
+
+
+def _load_snippets(generator: str, transformer: str) -> Sequence[Snippet]:
+  if generator == 'human':
+    with jsonlines.open(f'data/CodeNet/result/codenet_{transformer}.jsonl') as reader:
+      authors = sorted((obj['src']['problem_id'], obj['src']['author_name']) for obj in reader)
+    id_to_authors = {id_: set(map(itemgetter(1), pairs)) for id_, pairs in groupby(authors, key=itemgetter(0))}
+    with jsonlines.open('data/CodeNet/dataset/codenet/human_codenet_solution.jsonl') as reader:
+      solutions = sorted((obj['id'], obj['code'], obj['author_name']) for obj in reader
+                         if obj['id'] in id_to_authors and obj['author_name'] in id_to_authors[obj['id']])
+    samples = [sample(list(triplets), 1)[0] for _, triplets in groupby(solutions, key=itemgetter(0))]
+    return [Snippet(*triplet) for triplet in samples]
+  with jsonlines.open(f'data/CodeNet/dataset/codenet/{generator}_codenet_in_out.jsonl') as reader:
     return [Snippet(snippet['id'], snippet['code']) for snippet in reader]
 
 
-def _load_variants(path: str, snippets: Sequence[Snippet], generator: str) -> Sequence[Snippet]:
-  with jsonlines.open(path) as reader:
-    objects = [obj for obj in reader if obj['src']['author_name'] == generator]
+def _load_variants(generator: str, transformer: str, snippets: Sequence[Snippet]) -> Sequence[Snippet]:
+  with jsonlines.open(f'data/CodeNet/result/codenet_{transformer}.jsonl') as reader:
+    if generator == 'human':
+      objects = list(reader)
+    else:
+      objects = [obj for obj in reader if obj['src']['author_name'] == generator]
   variants = [None] * len(snippets)
   for i, snippet in enumerate(snippets):
-    target = next((obj for obj in objects if obj['src']['problem_id'] == snippet.id), None)
+    target = next((obj for obj in objects if obj['src']['problem_id'] == snippet.id and (generator != 'human' or obj['src']['author_name'] == snippet.ref)), None)
     if target:
       variants[i] = Snippet(snippet.id, target['result']['file_name'])
   return variants
@@ -28,11 +93,9 @@ def _load_variants(path: str, snippets: Sequence[Snippet], generator: str) -> Se
 def main():
   args = parse_args()
   dataset = 'CodeNet'
-  generator = 'claude35sonnet'
-  transformer = 'deepseekcoder'
-  logger.info(f'Evaluating {dataset} from {generator} to {transformer}...')
-  snippets = _load_snippets(f'data/CodeNet/dataset/codenet/{generator}_codenet_in_out.jsonl')
-  variants = _load_variants(f'data/CodeNet/result/codenet_{transformer}.jsonl', snippets, generator)
+  logger.info(f'Evaluating {dataset} from {args.generator} to {args.transformer}...')
+  snippets = _load_snippets(args.generator, args.transformer)
+  variants = _load_variants(args.generator, args.transformer, snippets)
   snippets = [snippet for snippet, variant in zip(snippets, variants) if variant is not None]
   variants = [variant for variant in variants if variant is not None]
   if args.num_snippets >= 0:
