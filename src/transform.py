b3b76609dd910e5234a5924dc3f1639d60b893ce
Fradow Liu <zhiyuanliu@smail.nju.edu.cn>
2025-05-20 08:41:34 +0800

    Span by spots

--- src/transform.py
+++ src/transform.py
@@ -1,4 +1,5 @@
 import os
+from itertools import chain
 from typing import Sequence
 
 import jpype as jp
@@ -13,22 +14,22 @@ with open('settings.yml', 'r') as f:
 
 
 class Mutator():
-  def __init__(self, src_lang, style_file):
+  def __init__(self, lang):
     jp.startJVM('-ea', jvmpath=os.getenv('JVM_PATH'),
                 classpath=[os.getenv('TSFM_CLASSPATH')])
-    self.src_lang = src_lang
-    self.instance = jp.JClass(config['class']).createMutator(src_lang, style_file)
-    if not self.instance:
-      raise ValueError(f'Failed to create a mutator with {style_file} in {src_lang}.')
+    self.lang = lang
+    self.cls = jp.JClass(config['class'])
+    if not self.cls:
+      raise ValueError('Failed to load the mutator class.')
 
   def __del__(self):
     jp.shutdownJVM()
 
-  def apply(self, snippets: Sequence[Snippet]) -> Sequence[Snippet]:
+  def apply(self, snippets: Sequence[Snippet], style_file: str) -> Sequence[Snippet]:
     variants = [None] * len(snippets)
     for i, snippet in tqdm(enumerate(snippets), desc='Transforming', total=len(snippets), leave=False):
       try:
-        variant = self.instance.apply(snippet.code)
+        variant = self.cls.apply(self.lang, snippet.code, style_file)
       except Exception as e:
         logger.error(f'Error occurred for snippet {snippet.id}:\n{e}')
         variant = None
@@ -39,6 +40,21 @@ class Mutator():
         variants[i] = snippet._replace(code=str(variant))
     return variants
 
+  def span(self, snippets: Sequence[Snippet]) -> Sequence[Sequence[Snippet]]:
+    variants_lists = [None] * len(snippets)
+    for i, snippet in tqdm(enumerate(snippets), desc='Spanning', total=len(snippets), leave=False):
+      try:
+        variants = self.cls.span(self.lang, snippet.code)
+      except Exception as e:
+        logger.error(f'Error occurred for snippet {snippet.id}:\n{e}')
+        variants = []
+      if not variants:
+        logger.warning(f'Failed to span {snippet.id}.')
+        variants_lists[i] = [snippet]
+      else:
+        variants_lists[i] = [snippet._replace(code=str(variant)) for variant in variants]
+    return list(chain.from_iterable(variants_lists))
+
 
 def transform_source(snippets: Sequence[Snippet], src_lang: str) -> Sequence[Snippet]:
   """
@@ -47,21 +63,12 @@ def transform_source(snippets: Sequence[Snippet], src_lang: str) -> Sequence[Sni
   :param src_lang: source language
   :return: the variant sequence
   """
-  logger.info('Applying transformations...')
-  style_file = generate_styles(src_lang)
   # TODO: load existing variants if available
   # TODO: multi-threading optimization
-  mutator = Mutator(src_lang, style_file)
-  return mutator.apply(snippets)
-
-
-def generate_styles(src_lang: str) -> str:
-  """
-  Generates rules for code transformation.
-  :param src_lang: source language
-  :return: the path to the rules file
-  """
-  # TODO:
-  #   1. given a code snippet, find all possible points to apply the rules
-  #   2. enumerate all possible combinations of the rules applied to the points above
-  return os.getenv('STYLE_FILE')
+  mutator = Mutator(src_lang)
+  style_file = os.getenv('STYLE_FILE')
+  if not style_file:
+    logger.info('Spanning styles...')
+    return mutator.span(snippets)
+  logger.info(f'Applying styles from {style_file}...')
+  return mutator.apply(src_lang, style_file)
