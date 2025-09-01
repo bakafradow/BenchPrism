5abed0e247bb8ca97e6b72671f6cbfe8e4ec5e68
Fradow Liu <zhiyuanliu@smail.nju.edu.cn>
2025-09-01 19:15:47 +0800

    Calculate coverage concurrently

--- src/stylo_flora/metrics/coverage.py
+++ src/stylo_flora/metrics/coverage.py
@@ -1,6 +1,7 @@
 import subprocess
 import tempfile
 from collections.abc import Sequence
+from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 
 import numpy as np
@@ -8,7 +9,7 @@ import pandas as pd
 import yaml
 from tqdm import tqdm
 
-from .. import Snippet
+from .. import IOTestCase, Snippet
 from ..logger import logger
 from .utils import extract_classname_java
 
@@ -34,17 +35,19 @@ def calc_coverage_java(snippet: Snippet) -> dict:
     jar_dir = Path('data').absolute()
     exec_name = 'test.exec'
     csv_name = 'coverage.csv'
-    num_pass = 0
-    for testcase in tqdm(snippet.args['io_testcases'], desc='Calculating coverage',
-                         total=len(snippet.args['io_testcases']), leave=False):
+    def worker(testcase: IOTestCase) -> bool:
       exec_cmd = f'java -javaagent:{jar_dir}/jacocoagent.jar=destfile={exec_name},append=true {classname}'.split()
       returned = subprocess.run(exec_cmd, cwd=temp_dir, input=testcase.input,
                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                 encoding='utf-8', timeout=config['timeout'])
       if returned.returncode != 0:
-        logger.warning(f'Failed to execute snippet {snippet.id} with jacocoagent.')
-        continue
-      num_pass += returned.stdout.strip() in (output.strip() for output in testcase.outputs)
+        logger.warning(f'Failed to execute snippet {snippet.id} with jacocoagent:\n{returned.stdout}')
+        return False
+      return returned.stdout.strip() in (output.strip() for output in testcase.outputs)
+
+    with ThreadPoolExecutor(max_workers=config['max_workers']) as executor:
+      num_pass = sum(tqdm(executor.map(worker, snippet.args['io_testcases']),
+                          total=len(snippet.args['io_testcases']), leave=False))
     pass_rate = num_pass / len(snippet.args['io_testcases'])
 
     report_cmd = f'java -jar {jar_dir}/jacococli.jar report {exec_name} --classfiles . --sourcefiles . --csv {csv_name}'.split()
@@ -61,11 +64,12 @@ def calc_coverage(snippets: Sequence[Snippet], lang: str) -> dict:
   coverage_func = globals().get(f'calc_coverage_{lang}')
   if not coverage_func:
     raise TypeError(f'Unsupported language: {lang}')
-  coverages = [coverage_func(snippet) for snippet in snippets]
+  coverages = list(tqdm((coverage_func(snippet) for snippet in snippets),
+                        desc='Calculating coverage', total=len(snippets)))
   return {
       'pass_rate': np.mean([cov['pass_rate'] for cov in coverages]),
       'line_cov_rate': np.mean([cov['LINE_COVERED'] / (cov['LINE_COVERED'] + cov['LINE_MISSED']) if (cov['LINE_COVERED'] + cov['LINE_MISSED']) > 0 else 1
-                                for cov in coverages]),
+                                for cov in coverages if cov]),
       'branch_cov_rate': np.mean([cov['BRANCH_COVERED'] / (cov['BRANCH_COVERED'] + cov['BRANCH_MISSED']) if (cov['BRANCH_COVERED'] + cov['BRANCH_MISSED']) > 0 else 1
-                                  for cov in coverages]),
+                                  for cov in coverages if cov]),
   }
